681fc2bd07c88a2cb4b3874e8d644a819f271743
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-04 03:11:44 +0300

    replace list in DAO to Postgres DB

--- src/main/java/org/example/dao/PersonsDAO.java
+++ src/main/java/org/example/dao/PersonsDAO.java
@@ -1,40 +1,116 @@
 package org.example.dao;
 
+import org.example.Property;
 import org.example.models.Person;
+import org.springframework.aop.scope.ScopedObject;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonsDAO {
-    private List<Person> persons;
-    {
-        persons = new ArrayList<>();
-        persons.add(new Person("john", "doe"));
-        persons.add(new Person("ivan", "ivanov"));
-        persons.add(new Person("Matthew", "McConaughey"));
-    }
-    public List<Person> getAllPersons(){
-        return this.persons;
-    }
-    public Person getPerson(int id){
-        return persons
-                .stream()
-                .filter(person -> person.getId() == id)
-                .findFirst()
-                .orElse(null);
-    }
-    public void add(Person person){
-        persons.add(person);
-    }
-    public void update(Person person, int id){
-        persons.set(id, person);
-    }
-    public void delete(int id){
-        persons.remove(id);
-        for (int i = id; i < persons.size(); i++) {
-            persons.get(i).setId(i);
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(Property.URL, Property.User, Property.Password);
+            // Property is a class of static fields in what contains sources of DB
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public List<Person> getAllPersons() {
+        List<Person> resultList = new ArrayList<>();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setFirstName(resultSet.getString("name"));
+                person.setSecondName(resultSet.getString("surname"));
+                resultList.add(person);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
+
+    public Person getPerson(int id) {
+        Statement statement = null;
+        Person person = new Person();
+        try {
+            statement = connection.createStatement();
+            String query = "SELECT * FROM Person " +
+                    "WHERE id =" + id + ";";
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            person.setFirstName(resultSet.getString("name"));
+            person.setSecondName(resultSet.getString("surname"));
+            person.setId(resultSet.getInt("id"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return person;
+    }
+
+    public void add(Person person) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "INSERT INTO person VALUES (default, '" + person.getFirstName() + "', '" + person.getSecondName() + "');";
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Person person, int id) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "UPDATE person "+
+                    "SET name = '"+person.getFirstName()+"', surname = '"+person.getSecondName()+"' " +
+                    "WHERE id ="+id+";";
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void delete(int id) {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "DELETE FROM person "+
+                    "WHERE id = "+id+" ;";
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getCount() {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            String query = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(query);
+            int result = 0;
+            for (; resultSet.next(); result++) {
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 1;
     }
 }
