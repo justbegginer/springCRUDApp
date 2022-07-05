e9d8a70e76cae36a5cc8fad948e3483c55cf1e70
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-06 00:46:47 +0300

    remake PersonsDAO.java, instead of JDBC Template i am using Hibernate

--- src/main/java/org/example/dao/PersonsDAO.java
+++ src/main/java/org/example/dao/PersonsDAO.java
@@ -1,10 +1,14 @@
 package org.example.dao;
 
+import org.example.models.HibernateSessionFactory;
 import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
 import java.util.List;
 
 @Component
@@ -21,24 +25,30 @@ public class PersonsDAO {
     }
 
     public Person getPerson(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person " +
-                "WHERE id = ?;", new Object[]{id},new PersonMapper())
-                .stream()
-                .findFirst()
-                .orElse(null);
+        return HibernateSessionFactory.getSessionFactory().openSession().get(Person.class, id);
     }
 
     public void add(Person person) {
-        jdbcTemplate.update("INSERT INTO person VALUES (default, ?, ?);",
-                person.getFirstName(), person.getSecondName());
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(person);
+        transaction.commit();
+        session.close();
     }
 
     public void update(Person person, int id) {
-        jdbcTemplate.update("UPDATE person " + "SET name = ?, surname = ? " + "WHERE id = ?;",
-                person.getFirstName(), person.getSecondName(), id);
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(person);
+        transaction.commit();
+        session.close();
     }
 
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM person " + "WHERE id = ? ;", id);
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(getPerson(id));
+        transaction.commit();
+        session.close();
     }
 }
