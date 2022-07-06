e845534d2e48fd046b76d0ce7d6f896eaf7cc185
justbegginer <blackcloudsinthesky@protonmail.com>
2022-07-07 02:36:06 +0300

    replace all getters, setters, constructor and toString methods to Lombok annotations

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,59 +1,23 @@
 package org.example.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "person")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String surname;
-
-    public Person(int id, String firstName, String secondName) {
-        this.id = id;
-        this.name = firstName;
-        this.surname = secondName;
-    }
-
-    public Person() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String firstName) {
-        this.name = firstName;
-    }
-
-    public void setSurname(String secondName) {
-        this.surname = secondName;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + name + '\'' +
-                ", secondName='" + surname + '\'' +
-                '}';
-    }
 
     public boolean equal(Person anotherPerson) {
         return anotherPerson.getId() == this.getId() &&
